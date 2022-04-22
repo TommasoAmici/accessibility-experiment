f055d4ffb0c4173d40216fa08d58bec88026b785
Tommaso Amici <me@tommasoamici.com>
2022-04-22 23:38:01 +0200

    feat: added ButtonLink component

---
+++ experiment/components/ButtonLink.tsx
@@ -0,0 +1,22 @@
+import classNames from "classnames";
+import { forwardRef } from "react";
+
+type AnchorProps = React.HTMLProps<HTMLAnchorElement>;
+
+const ButtonLink = forwardRef<HTMLAnchorElement, AnchorProps>((props, ref) => {
+  return (
+    <a
+      ref={ref}
+      {...props}
+      className={classNames(
+        "bg-black px-3 py-1.5 text-white no-underline outline-none",
+        "transition-colors hover:bg-neutral-800 focus:ring-4 focus:ring-black focus:ring-offset-4",
+        props.className,
+      )}
+    >
+      {props.children}
+    </a>
+  );
+});
+ButtonLink.displayName = "ButtonLink";
+export default ButtonLink;
