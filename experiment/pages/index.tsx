1b779ef469cdc1b4b8d4ca1d1c6beb8eecd29118
Tommaso Amici <me@tommasoamici.com>
2022-04-22 23:36:09 +0200

    fix: added start button to home page

--- experiment/pages/index.tsx
+++ experiment/pages/index.tsx
@@ -1,36 +1,45 @@
-import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import ButtonLink from "../components/ButtonLink";
+import { usePressEnter } from "../hooks/usePressEnter";
 
-const Home = () => {
+const HomePage = () => {
+  const router = useRouter();
+  const nextURL = "/shop";
+
+  usePressEnter(nextURL, router);
+
   return (
-    <>
-      <Head>
-        <title>Thesis</title>
-      </Head>
-
-      <main className="prose mx-auto mt-8">
-        <section>
-          <p>
-            Hello there, thanks for taking the time to participate. I&apos;m Tommaso Amici, a web
-            developer and a master student at the University of Amsterdam.
-          </p>
-          <p>
-            For my thesis, I&apos;m researching the effects of web accessibility on business
-            performance.
-          </p>
-          <p>
-            If you decide to participate, I will ask you to perform a small number of taks and to
-            answer some general questions about yourself and the experiment.
-          </p>
-          <p>
-            Lastly, to thank you for your time, you can optionally enter a lottery and I will
-            randomly pick a participant to win 25€.
-          </p>
-        </section>
-      </main>
-
-      <footer className=""></footer>
-    </>
+    <section className="mx-auto prose prose-lg">
+      <p>
+        Hello there, thanks for taking the time to participate. I&apos;m Tommaso Amici, a web
+        developer and a master student at the University of Amsterdam.
+      </p>
+      <p>
+        For my thesis, I&apos;m researching the effects of web accessibility on consumer behavior.
+      </p>
+      <p>
+        If you decide to participate, I will ask you to perform a small number of tasks and to
+        answer some questions about yourself and the experiment. I expect it will take about{" "}
+        <strong>
+          <time dateTime="5m">five minutes</time>
+        </strong>{" "}
+        to complete.
+      </p>
+      <p>
+        Lastly, to thank you for your time, you can optionally enter a lottery and I will randomly
+        pick a participant to win 25€.
+      </p>
+      <div className="relative flex flex-col w-full">
+        <Link href={nextURL} passHref>
+          <ButtonLink className="mx-auto mt-4">Start</ButtonLink>
+        </Link>
+        <p className="mx-auto text-sm">
+          press <kbd className="font-sans font-semibold">Enter ↵</kbd>
+        </p>
+      </div>
+    </section>
   );
 };
 
-export default Home;
+export default HomePage;
