d4dc8e791d242b4f74fe1b4a1150619196a1bafe
Tommaso Amici <me@tommasoamici.com>
2022-04-25 10:42:26 +0200

    feat: added Button component

---
+++ experiment/components/Button.tsx
@@ -0,0 +1,22 @@
+import classNames from "classnames";
+import { forwardRef } from "react";
+
+type ButtonProps = JSX.IntrinsicAttributes &
+  React.ClassAttributes<HTMLButtonElement> &
+  React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
+  return (
+    <button
+      ref={ref}
+      {...props}
+      className={classNames(
+        "bg-black px-3 py-1.5 text-white no-underline outline-none transition-colors hover:bg-neutral-800 focus:ring-4 focus:ring-black focus:ring-offset-4 disabled:cursor-not-allowed disabled:bg-black/50",
+        props.className,
+      )}
+    >
+      {props.children}
+    </button>
+  );
+});
+Button.displayName = "Button";
