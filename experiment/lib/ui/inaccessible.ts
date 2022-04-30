546f63cfe301362d9a3a15620fdb3a2fb31f29c3
Tommaso Amici <me@tommasoamici.com>
2022-04-30 14:17:45 +0200

    feat: added inaccessible colors

--- experiment/lib/ui/inaccessible.ts
+++ experiment/lib/ui/inaccessible.ts
@@ -1,53 +1,40 @@
 const bgFromColor: ProductUI = {
-  green: "bg-green-800 hover:bg-green-600 focus:bg-green-800",
-  purple: "bg-fuchsia-900 hover:bg-fuchsia-700 focus:bg-fuchsia-900",
-  white: "bg-blue-800 hover:bg-blue-600 focus:bg-blue-800",
-  black: "bg-black hover:bg-neutral-700 focus:bg-black",
-  red: "bg-red-900 hover:bg-red-700 focus:bg-red-900",
-  blue: "bg-blue-800 hover:bg-blue-600 focus:bg-blue-800",
-  yellow: "bg-yellow-400 text-black hover:bg-yellow-300 focus:bg-yellow-400",
-  orange: "bg-orange-400 text-white hover:bg-orange-300",
-  beige: "bg-[#e5cfb2] text-black hover:bg-[#f9e2c8]",
-  gray: "bg-neutral-700 text-white hover:bg-neutral-500",
+  green: "bg-green-400 hover:bg-green-300",
+  purple: "bg-fuchsia-400 hover:bg-fuchsia-300",
+  white: "bg-blue-800 hover:bg-blue-600",
+  black: "bg-neutral-400 hover:bg-neutral-300",
+  red: "bg-red-500 hover:bg-red-400",
+  blue: "bg-blue-400 hover:bg-blue-300",
+  yellow: "bg-yellow-400 hover:bg-yellow-300",
+  orange: "bg-orange-400 hover:bg-orange-300",
+  beige: "bg-[#e5cfb2] hover:bg-[#f9e2c8]",
+  gray: "bg-neutral-700 hover:bg-neutral-500",
 };
 
 const borderFromColor: ProductUI = {
-  green: "border-green-600",
-  purple: "border-fuchsia-800",
-  white: "border-blue-700",
-  black: "border-neutral-800",
-  red: "border-red-600",
-  blue: "border-blue-700",
-  yellow: "border-yellow-500",
-  orange: "border-orange-700",
-  beige: "border-[#533d23]",
+  green: "border-green-300",
+  purple: "border-fuchsia-300",
+  white: "border-blue-300",
+  black: "border-neutral-400",
+  red: "border-red-300",
+  blue: "border-blue-300",
+  yellow: "border-yellow-200",
+  orange: "border-orange-400",
+  beige: "border-[#e5cfb2]",
   gray: "border-neutral-600",
 };
 
-const focusRingFromColor: ProductUI = {
-  green: "focus:ring-green-600",
-  purple: "focus:ring-fuchsia-700",
-  white: "focus:ring-black",
-  black: "focus:ring-black",
-  red: "focus:ring-red-500",
-  blue: "focus:ring-blue-600",
-  yellow: "focus:ring-yellow-600",
-  orange: "focus:ring-orange-700",
-  beige: "focus:ring-black",
-  gray: "focus:ring-neutral-600",
-};
-
 const checkboxFromColor: ProductUI = {
-  green: "bg-green-500 text-green-500 black-checkmark",
+  green: "bg-green-500 text-green-500",
   purple: "bg-fuchsia-800 text-fuchsia-800",
-  white: "bg-white text-white border border-black black-checkmark",
+  white: "bg-white text-white border border-neutral-400",
   black: "bg-black text-black",
   red: "bg-red-700 text-red-700",
   blue: "bg-blue-700 text-blue-700",
-  yellow: "bg-yellow-400 text-yellow-400 black-checkmark",
-  orange: "bg-orange-400 text-orange-400 black-checkmark",
-  beige: "bg-[#e5cfb2] text-[#e5cfb2] black-checkmark",
-  gray: "bg-neutral-400 text-neutral-400 black-checkmark",
+  yellow: "bg-yellow-400 text-yellow-400",
+  orange: "bg-orange-400 text-orange-400",
+  beige: "bg-[#e5cfb2] text-[#e5cfb2]",
+  gray: "bg-neutral-400 text-neutral-400",
 };
 
 const colorFromLevel = {
@@ -60,7 +47,6 @@ const colorFromLevel = {
 export const inaccessible = {
   bgFromColor,
   borderFromColor,
-  focusRingFromColor,
   checkboxFromColor,
   colorFromLevel,
 };
