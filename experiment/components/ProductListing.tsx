a216750c95d6cabce74181afe459506d60bc873c
Tommaso Amici <me@tommasoamici.com>
2022-04-30 14:17:18 +0200

    fix: if filters return no products show text explaining no products were found

--- experiment/components/ProductListing.tsx
+++ experiment/components/ProductListing.tsx
@@ -7,11 +7,15 @@ interface ComponentProps {
 const Accessible = ({ products }: ComponentProps) => {
   return (
     <ul className="grid grid-cols-1 gap-4 gap-y-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3">
-      {products.map(product => (
-        <li key={product.name}>
-          <ProductCard accessible={true} product={product} />
-        </li>
-      ))}
+      {products.length === 0 ? (
+        <li>No products matching these filters</li>
+      ) : (
+        products.map(product => (
+          <li key={product.name}>
+            <ProductCard accessible={true} product={product} />
+          </li>
+        ))
+      )}
     </ul>
   );
 };
@@ -19,9 +23,13 @@ const Accessible = ({ products }: ComponentProps) => {
 const Inaccessible = ({ products }: ComponentProps) => {
   return (
     <div className="grid grid-cols-1 gap-4 gap-y-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3">
-      {products.map(product => (
-        <ProductCard accessible={false} product={product} key={product.name} />
-      ))}
+      {products.length === 0 ? (
+        <p className="w-full text-neutral-400">No products matching these filters</p>
+      ) : (
+        products.map(product => (
+          <ProductCard accessible={false} product={product} key={product.name} />
+        ))
+      )}
     </div>
   );
 };
