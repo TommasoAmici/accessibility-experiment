9759ce0ca5b5ba3bf941c7e56ce1e1fa221fd6bf
Tommaso Amici <me@tommasoamici.com>
2022-05-04 15:25:48 +0200

    docs: added explanation to useCompleteExperiment

--- experiment/hooks/useCompleteExperiment.tsx
+++ experiment/hooks/useCompleteExperiment.tsx
@@ -2,6 +2,9 @@ import { useRouter } from "next/router";
 import { useContext, useEffect } from "react";
 import StateContext from "../contexts/state";
 
+/**
+ * A hook to redirect users to the start of the experiment if they haven't completed it
+ */
 export const useCompleteExperiment = () => {
   const router = useRouter();
   const { taskStartedAt, taskFinishedAt, addNotification } = useContext(StateContext);
