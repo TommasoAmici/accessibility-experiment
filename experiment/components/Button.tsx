bcb87e9bf1557ac89fb0ffdf39f599a88e2c7a70
Tommaso Amici <me@tommasoamici.com>
2022-05-23 20:07:29 +0200

    feat: added ButtonSecondary

--- experiment/components/Button.tsx
+++ experiment/components/Button.tsx
@@ -20,3 +20,19 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) =>
   );
 });
 Button.displayName = "Button";
+
+export const ButtonSecondary = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
+  return (
+    <button
+      ref={ref}
+      {...props}
+      className={classNames(
+        "border-4 border-black px-3 py-1.5 no-underline outline-none transition-colors focus:ring-4 focus:ring-black focus:ring-offset-4 disabled:cursor-not-allowed disabled:bg-black/50",
+        props.className,
+      )}
+    >
+      {props.children}
+    </button>
+  );
+});
+ButtonSecondary.displayName = "ButtonSecondary";
